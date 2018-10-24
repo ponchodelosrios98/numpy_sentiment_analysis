96c70ed7a913e40768380ea23f43662779e4973a
ponchodelosrios98 <ponchodelosrios98@gmail.com>
2018-10-24 00:42:13 -0700

    Added epochs

--- train.py
+++ train.py
@@ -142,7 +142,7 @@ class SentimentNetwork:
     def sigmoid_output_2_derivative(self,output):
         return output * (1 - output)
     
-    def train(self, training_reviews_raw, training_labels):
+    def train(self, training_reviews_raw, training_labels, epochs):
         # Init training reviews
         training_reviews = list()
 
@@ -164,55 +164,52 @@ class SentimentNetwork:
         # Remember when we started for printing time statistics
         start = time.time()
         
-        # loop through all the given reviews and run a forward and backward pass,
-        # updating weights for every item
-        for i in range(len(training_reviews)):
-            
-            # Get the next review and its correct label
-            review = training_reviews[i]
-            label = training_labels[i]
-            
-            # Hidden layer
-            self.layer_1 *= 0
+        # Loop Epoch times
+        for epoch in range(epochs):
+            # loop through all the given reviews and run a forward and backward pass,
+            # updating weights for every item
+            for i in range(len(training_reviews)):
+                # Get the next review and its correct label
+                review = training_reviews[i]
+                label = training_labels[i]
+                
+                # Hidden layer
+                self.layer_1 *= 0
 
-            # Layer_1 Grabs the weights of given index
-            for index in review:
-                self.layer_1 += self.weights_0_1[index]
+                # Layer_1 Grabs the weights of given index
+                for index in review:
+                    self.layer_1 += self.weights_0_1[index]
 
-            # OActivation Function between weights and inputs
-            layer_2 = self.sigmoid(self.layer_1.dot(self.weights_1_2))            
-            
-            # Output error
-            layer_2_error = layer_2 - self.get_target_for_label(label) # Output layer error is the difference between desired target and actual output.
-            layer_2_delta = layer_2_error * self.sigmoid_output_2_derivative(layer_2)
+                # OActivation Function between weights and inputs
+                layer_2 = self.sigmoid(self.layer_1.dot(self.weights_1_2))            
+                
+                # Output error
+                layer_2_error = layer_2 - self.get_target_for_label(label) # Output layer error is the difference between desired target and actual output.
+                layer_2_delta = layer_2_error * self.sigmoid_output_2_derivative(layer_2)
 
-            # Backpropagated error
-            layer_1_error = layer_2_delta.dot(self.weights_1_2.T) # errors propagated to the hidden layer
-            layer_1_delta = layer_1_error # hidden layer gradients - no nonlinearity so it's the same as the error
+                # Backpropagated error
+                layer_1_error = layer_2_delta.dot(self.weights_1_2.T) # errors propagated to the hidden layer
+                layer_1_delta = layer_1_error # hidden layer gradients - no nonlinearity so it's the same as the error
 
-            # Update the weights
-            self.weights_1_2 -= self.layer_1.T.dot(layer_2_delta) * self.learning_rate # update hidden-to-output weights with gradient descent step
-            
-            for index in review:
-                self.weights_0_1[index] -= layer_1_delta[0] * self.learning_rate # update input-to-hidden weights with gradient descent step
+                # Update the weights
+                self.weights_1_2 -= self.layer_1.T.dot(layer_2_delta) * self.learning_rate # update hidden-to-output weights with gradient descent step
+                
+                for index in review:
+                    self.weights_0_1[index] -= layer_1_delta[0] * self.learning_rate # update input-to-hidden weights with gradient descent step
 
-            # Keep track of correct predictions.
-            if(layer_2 >= 0.5 and label == 'POSITIVE'):
-                correct_so_far += 1
-            elif(layer_2 < 0.5 and label == 'NEGATIVE'):
-                correct_so_far += 1
-            
-            # For debug purposes, print out our prediction accuracy and speed 
-            # throughout the training process. 
-            elapsed_time = float(time.time() - start)
-            reviews_per_second = i / elapsed_time if elapsed_time > 0 else 0
-            
-            sys.stdout.write("\rProgress:" + str(100 * i/float(len(training_reviews)))[:4] \
-                             + "% Speed(reviews/sec):" + str(reviews_per_second)[0:5] \
-                             + " #Correct:" + str(correct_so_far) + " #Trained:" + str(i+1) \
-                             + " Training Accuracy:" + str(correct_so_far * 100 / float(i+1))[:4] + "%")
-            if(i % 2500 == 0):
-                print("")
+                # Keep track of correct predictions.
+                if(layer_2 >= 0.5 and label == 'POSITIVE'):
+                    correct_so_far += 1
+                elif(layer_2 < 0.5 and label == 'NEGATIVE'):
+                    correct_so_far += 1
+                
+                # For debug purposes, print out our prediction accuracy and speed 
+                # throughout the training process. 
+                elapsed_time = float(time.time() - start)
+                reviews_per_second = i / elapsed_time if elapsed_time > 0 else 0
+            print("\rEpoch:" + str(epoch)
+                + " #Trained:" + str(((i+1) * (epoch + 1))) \
+                + " Training Accuracy:" + str(correct_so_far * 100 / float((i+1) * (epoch + 1)))[:4] + "%")
     
     def test(self, testing_reviews, testing_labels):
         """
@@ -287,7 +284,7 @@ mlp = SentimentNetwork(reviews[:-1000],labels[:-1000],min_count=20,polarity_cuto
 print('--------')
 print('TRAINING:')
 print('--------')
-mlp.train(reviews[:-1000],labels[:-1000])
+mlp.train(reviews[:-1000],labels[:-1000], 5)
 print('--------')
 print('TESTING:')
 print('--------')
